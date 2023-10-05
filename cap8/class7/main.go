d7ee15de3837090210bdc53a4a729b762e9b1aa9
MatheusVict <matheusvictorhenrique@gmail.com>
2023-10-05 18:47:09 -0300

    feat: multidimensional
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- cap8/class7/main.go
+++ cap8/class7/main.go
@@ -1,5 +1,36 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	sliceOfSlice := [][]int{
+		[]int{1, 2, 3},
+		[]int{4, 5, 6},
+		[]int{7, 8, 9},
+	}
+
+	fmt.Println(sliceOfSlice)
+	fmt.Println(sliceOfSlice[1])
+	fmt.Println(sliceOfSlice[0][2])
+
+	sliceSuper := [][][][]int{
+		[][][]int{
+			[][]int{
+				[]int{1, 2, 3, 4},
+			},
+			[][]int{
+				[]int{4, 5, 6, 6},
+			},
+		},
+
+		[][][]int{
+			[][]int{
+				[]int{9, 8, 7},
+			},
+		},
+	}
+
+	fmt.Println(sliceSuper[0][1][0][1])
 
 }
