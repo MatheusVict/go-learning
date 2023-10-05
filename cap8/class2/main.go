7e709c7e00aabed69a7923bde01cc5c2d44fd6c6
MatheusVict <matheusvictorhenrique@gmail.com>
2023-10-04 22:57:31 -0300

    feat: slice
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- cap8/class2/main.go
+++ cap8/class2/main.go
@@ -1,5 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
+	array := [5]int{1, 2, 3, 4, 5}
+	fmt.Println(array)
+	slice := []int{1, 2, 3, 4, 5}
+	fmt.Println(append(slice, 6))
+	slice[3] = 45331
+
+	fmt.Println(slice[3])
 
 }
