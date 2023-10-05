ffdcc2afccde6ffafedce752c3a328f4ff675902
MatheusVict <matheusvictorhenrique@gmail.com>
2023-10-05 19:47:31 -0300

    feat: exercise 1
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

---
+++ cap9/exercise1/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	array := [5]int{1, 2, 3, 4, 5}
+
+	fmt.Println(array)
+
+	for i := range array {
+		fmt.Println(i)
+	}
+
+	fmt.Printf("%T", array)
+}
