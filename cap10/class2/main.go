c739804a24b08f2b990a0741dc4aa993628c9ad1
MatheusVict <matheusvictorhenrique@gmail.com>
2023-10-05 21:50:11 -0300

    feat: class2
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

---
+++ cap10/class2/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type person struct {
+	name string
+	age  int
+}
+
+type proficional struct {
+	person
+	title   string
+	salario int
+}
+
+func main() {
+	person1 := person{name: "joão", age: 88}
+	person2 := person{name: "felip", age: 12}
+	person3 := person{name: "mané", age: 95}
+
+	prossional1 := proficional{title: "padeiro", salario: 61532, person: person1}
+	prossional2 := proficional{title: "çflasd", salario: 61532, person: person2}
+	prossional3 := proficional{title: "çfjalds", salario: 61532, person: person3}
+
+	fmt.Println(prossional1)
+	fmt.Println(prossional2)
+	fmt.Println(prossional3)
+}
