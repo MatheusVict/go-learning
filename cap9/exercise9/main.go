dfb105947098f767a609173af364718af6e4b3d0
MatheusVict <matheusvictorhenrique@gmail.com>
2023-10-05 21:29:48 -0300

    feat: exercise9
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

---
+++ cap9/exercise9/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	map1 := map[string][]string{
+		"henrique_matheus": []string{"arroz", "feijão", "macarrão"},
+		"Tú":               []string{"pedir carinho", "comer", "dormir"},
+		"Nata":             []string{"aperriar", "chorar", "atormentar"},
+		"SHirley":          []string{"arroz", "feijão", "macarrão"},
+	}
+
+	map1["arroz"] = []string{"feijão"}
+
+	for s, _ := range map1 {
+		fmt.Println(s, map1[s])
+	}
+}
