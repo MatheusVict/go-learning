9518e7913ac7bb48c87df27435ae5689bfd219de
MatheusVict <matheusvictorhenrique@gmail.com>
2023-10-13 11:22:24 -0300

    feat: mutex
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- cap18/class3/main.go
+++ cap18/class3/main.go
@@ -1,5 +1,45 @@
 package main
 
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var group = sync.WaitGroup{}
+
+// go run -race main.go
 func main() {
+	fmt.Println("CPUs: ", runtime.NumCPU())
+	fmt.Println("Goroutine: ", runtime.NumGoroutine())
+
+	count := 0
+	totalGoRoutines := 10
+
+	group.Add(totalGoRoutines)
+
+	var mu sync.Mutex
+
+	for i := 0; i < totalGoRoutines; i++ {
+		go func() {
+			// lock
+			mu.Lock()
+			value := count
+
+			//yield
+			runtime.Gosched()
+
+			value++
+			count = value
+			// unlock
+			mu.Unlock()
+			group.Done()
+		}()
+		fmt.Println("Goroutine: ", runtime.NumGoroutine())
+	}
+
+	group.Wait()
+	fmt.Println("Goroutine: ", runtime.NumGoroutine())
+	fmt.Println("count: ", count)
 
 }
