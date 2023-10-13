f75aec2ec19405252a1eed74118c32464cd27750
MatheusVict <matheusvictorhenrique@gmail.com>
2023-10-13 11:37:58 -0300

    feat: atomic
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

---
+++ cap18/class4/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"sync/atomic"
+)
+
+var group = sync.WaitGroup{}
+
+// go run -race main.go
+func main() {
+	var count int64
+	count = 0
+	totalGoRoutines := 10
+
+	group.Add(totalGoRoutines)
+
+	for i := 0; i < totalGoRoutines; i++ {
+		go func() {
+			atomic.AddInt64(&count, 1)
+			// lock
+
+			//yield
+			runtime.Gosched()
+
+			// unlock
+			fmt.Println("Count\t", atomic.LoadInt64(&count))
+			group.Done()
+		}()
+		fmt.Println("Goroutine: ", runtime.NumGoroutine())
+	}
+
+	group.Wait()
+	fmt.Println("count: ", count)
+
+}
