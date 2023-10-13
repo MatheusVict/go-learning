8d6862255b600b84df147b0ff5682d70c7033524
MatheusVict <matheusvictorhenrique@gmail.com>
2023-10-13 11:13:45 -0300

    feat: race condition
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

---
+++ cap18/class2/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var group = sync.WaitGroup{}
+
+// go run -race main.go
+func main() {
+	fmt.Println("CPUs: ", runtime.NumCPU())
+	fmt.Println("Goroutine: ", runtime.NumGoroutine())
+
+	count := 0
+	totalGoRoutines := 10
+
+	group.Add(totalGoRoutines)
+
+	for i := 0; i < totalGoRoutines; i++ {
+		go func() {
+			value := count
+
+			//yield
+			runtime.Gosched()
+
+			value++
+			count = value
+
+			group.Done()
+		}()
+		fmt.Println("Goroutine: ", runtime.NumGoroutine())
+	}
+
+	group.Wait()
+	fmt.Println("Goroutine: ", runtime.NumGoroutine())
+	fmt.Println("count: ", count)
+
+}
