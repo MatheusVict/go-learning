204b76855d6a3fa662f8d21ca5aa72445550d07b
MatheusVict <matheusvictorhenrique@gmail.com>
2023-10-16 18:50:09 -0300

    feat: dynamic docs file
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

---
+++ cap25/class5/doc.go
@@ -0,0 +1,6 @@
+/*
+Package wrinting show how to make a good documentation
+
+	It does a buch of nothing :)
+*/
+package wrinting
---
+++ cap25/class5/main.go
@@ -0,0 +1,20 @@
+package wrinting
+
+import "fmt"
+
+const het = 10
+
+// Doc is a useless function
+func Doc() {
+	fmt.Println("It doesn't do anything")
+}
+
+// Doc1 is another useless function that doesn't do anything
+func Doc1() {
+	fmt.Println("It doesn't do anything")
+}
+
+// Doc2 is another useless function that doesn't do anything
+func Doc2() {
+	fmt.Println("It doesn't do anything")
+}
