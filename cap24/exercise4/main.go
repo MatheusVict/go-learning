da449938f92f2494a3c5ff9f10477db100ca5662
MatheusVict <matheusvictorhenrique@gmail.com>
2023-10-16 15:42:46 -0300

    feat: exercise 4
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

---
+++ cap24/exercise4/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+type norgateMathError struct {
+	lat  string
+	long string
+	err  error
+}
+
+func (n norgateMathError) Error() string {
+	return fmt.Sprintf("A norgate math error occured: %v %v %v: ", n.lat, n.long, n.err)
+}
+
+func main() {
+	_, err := sqrt(-10)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func sqrt(f float64) (float64, error) {
+	if f < 0 {
+		ErrrNorgateMath := fmt.Errorf("nor-gate math: square root of negative number: %v", f)
+
+		return 0, norgateMathError{long: "50.1231 N", lat: "99.5562 W", err: ErrrNorgateMath}
+	}
+	return 42, nil
+}
